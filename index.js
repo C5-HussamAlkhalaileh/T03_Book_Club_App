9d1332cbb795819d296dd846dd09aa920f757120
HussamAlkhalaileh <alkhalaileh.hussam@gmail.com>
2022-07-25 22:01:48 +0300

    added backend and front end files

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
-const express=require("express");
-const app =express();
+const express = require("express");
+require("dotenv").config();
+const app = express();
 
-const   PORT =5000;
-app.listen(PORT,()=>{
-console.log("server is working port is "+PORT);
+const PORT = 5000;
+app.listen(PORT, () => {
+  console.log("server is working port is " + PORT);
 });
