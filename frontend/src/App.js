b28fb195290efc0b5da28ee59fab12f743de1921
HussamAlkhalaileh <alkhalaileh.hussam@gmail.com>
2022-07-28 22:09:49 +0300

    add login component front end

---
+++ frontend/src/App.js
@@ -0,0 +1,19 @@
+
+ 
+import React from "react";
+import "./App.css";
+
+//import { Routes, Route, Link } from "react-router-dom";
+import Home from './component/home'; 
+
+const App = () => {
+  return (
+    <>
+    
+    <Home/>
+    
+    </>
+  );
+};
+
+export default App;
--- frontend/src/component/register/index.js
+++ frontend/src/component/register/index.js
@@ -1,60 +1,60 @@
-import React, { useState ,useContext} from "react";
+//import React, { useState ,useContext} from "react";
 
-const Register = () => {
-  const [firstname, setfirsname] = useState("");
-  const [lastname, setlastname] = useState("");
-  const [email, setemail] = useState("");
-  const [password, setpassword] = useState("");
+//const Register = () => {
+//  const [firstname, setfirsname] = useState("");
+//  const [lastname, setlastname] = useState("");
+//  const [email, setemail] = useState("");
+//  const [password, setpassword] = useState("");
 
-  return (
-    <div>
-      <h3> Register </h3>
-
-      
-      <p>firstname: {setfirsname}</p>
-      <input
-        type="text"
-        placeHolder="firstname"
-        onChange={(e) => {
-          setfirsname(e.target.value);
-        }}
-      />
-
-      <p>lastname: {setlastname}</p>
-      <input
-        type="text"
-        placeHolder="lastname"
-        onChange={(e) => {
-          setfirsname(e.target.value);
-        }}
-      />
-      <p>email: {setemail}</p>
-
-      <p>email: {email}</p>
-      <input
-        type="email"
-        placeHolder="email"
-        onChange={(e) => {
-            setemail(e.target.value);
-        }}
-      />
-
-      <p>password: {setpassword}</p>
+//  return (
+//    <div>
+//      <h3> Register </h3>
 
       
-      <p>email: {password}</p>
-      <input
-        type="password"
-        placeHolder="password"
-        onChange={(e) => {
-            setpassword(e.target.value);
-        }}
-      />
+//      <p>firstname: {setfirsname}</p>
+//      <input
+//        type="text"
+//        placeHolder="firstname"
+//        onChange={(e) => {
+//          setfirsname(e.target.value);
+//        }}
+//      />
 
-      <button>Register</button>
-    </div>
-  );
-};
+//      <p>lastname: {setlastname}</p>
+//      <input
+//        type="text"
+//        placeHolder="lastname"
+//        onChange={(e) => {
+//          setfirsname(e.target.value);
+//        }}
+//      />
+//      <p>email: {setemail}</p>
+
+//      <p>email: {email}</p>
+//      <input
+//        type="email"
+//        placeHolder="email"
+//        onChange={(e) => {
+//            setemail(e.target.value);
+//        }}
+//      />
+
+//      <p>password: {setpassword}</p>
+
+      
+//      <p>email: {password}</p>
+//      <input
+//        type="password"
+//        placeHolder="password"
+//        onChange={(e) => {
+//            setpassword(e.target.value);
+//        }}
+//      />
+
+//      <button>Register</button>
+//    </div>
+//  );
+//};
 
 
-export default Register ;
+//export default Register ;
